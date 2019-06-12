f60becd09561c3c527b4937631c2056fd998df48
Eadral <a250014238@me.com>
2019-06-12 16:32:51 +0800

    nothing

--- test.sh
+++ test.sh
@@ -17,10 +17,11 @@ echo STAMP: $stamp >> result
 date >> result
 
 # test HERE
+./FYArray.exe | grep "The programe elapsed" >> result
 #hpcrun -t -e CPUTIME@5000 -e CYCLES -e INSTRUCTIONS@4000000 -e CACHE-MISSES ./FYArray.exe >> result
 #hpcstruct ./FYArray.exe
 #hpcprof -S FYArray.exe.hpcstruct -I ./+ hpctoolkit-FYArray.exe-measurements -o "./database-$stamp"
-amplxe-cl -collect hotspots -r "r-$stamp-$1-hotspots" -search-dir . ./FYArray.exe | grep "The programe elapsed" >> result
+amplxe-cl -collect hotspots -r "r-$stamp-$1-hotspots" -search-dir . ./FYArray.exe 
 #amplxe-cl -collect memory-consumption -r "r-$stamp-$1-memory-consumption" -search-dir . ./FYArray.exe
 #amplxe-cl -collect hpc-performance -r "r-$stamp-$1-hpc-performance" -search-dir . ./FYArray.exe 
 #amplxe-cl -collect general-exploration -r "r-$stamp-$1-general-exploration" -search-dir . ./FYArray.exe
